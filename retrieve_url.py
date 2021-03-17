34bfa90a37effa84805ecaf2bb7a737a8fba0794
RAlexandru <razvan_alex97@yahoo.com>
2021-03-17 19:13:31 +0200

    Modifications to name of the functions and prints

--- retrieve_url.py
+++ retrieve_url.py
@@ -6,7 +6,7 @@ class RetrieveUrl:
     @classmethod
     def retrieve_url(cls, path):
         url = "https://official-joke-api.appspot.com"
-        result = requests.get(url + path,verify=False)
+        result = requests.get(url + path)
         try:
             # result = requests.get(url + path)
             # print(result.status_code)
