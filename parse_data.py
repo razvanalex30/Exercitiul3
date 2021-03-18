167c34a92a958619ab63518568b851ad108bdc29
RAlexandru <razvan_alex97@yahoo.com>
2021-03-18 14:02:56 +0200

    Modified the Input actions from the user

--- parse_data.py
+++ parse_data.py
@@ -1,5 +1,6 @@
 # import requests
 from retrieve_url import RetrieveUrl
+from testus import AskInput
 
 
 def get_formatted_data(retrieved_data):
@@ -27,8 +28,12 @@ def get_formatted_data(retrieved_data):
 class ParseData:
 
     @classmethod
-    def parse_data(cls, choice, typee=None, nr=None):
+    def parse_data(cls):
+        AskInput.ask_choice()
         data = None
+        choice = AskInput.choice
+        typee = AskInput.type
+        nr = AskInput.chosen_number
         if choice == 1:
             data = RetrieveUrl.retrieve_endpoint_1()
         elif choice == 2:
@@ -39,43 +44,46 @@ class ParseData:
         print(jokes_formatted)
 
 
-print("""
-Hello! Please choose one of the following three inputs:
-1 - One random joke
-2 - Ten random joke
-3 - One/Ten random jokes by type
-""")
-while True:
-    try:
-        chosen_input = int(input("Enter your choice [1/2/3]: "))
-        if chosen_input == 1:
-            print("You have chosen One random joke!")
-            ParseData.parse_data(chosen_input)
-            break
-        elif chosen_input == 2:
-            print("You have chosen Ten random jokes!")
-            ParseData.parse_data(chosen_input)
-            break
-        elif chosen_input == 3:
-            types = ["general", "programming", "knock-knock"]
-            numbers = [1, 10]
-            type_value = None
-            number = None
-            while type_value not in types:
-                type_value = str(input("Please choose a type [general|programming|knock-knock]: "))
-                if type_value in types:
-                    break
-                else:
-                    print("Invalid type! Please choose a correct value\n")
-            print("Your type chosen was {}".format(type_value))
-            while number not in numbers:
-                number = int(input("Please choose the number of jokes [1/10]: "))
-                if number in numbers:
-                    break
-                else:
-                    print("Invalid! Please choose 1 or 10")
-            ParseData.parse_data(chosen_input, type_value, number)
-            break
-        print("Please enter a valid number!\n")
-    except Exception as e:
-        print(e, "Please choose a valid input!\n")
+# print("""
+# Hello! Please choose one of the following three inputs:
+# 1 - One random joke
+# 2 - Ten random joke
+# 3 - One/Ten random jokes by type
+# """)
+# while True:
+#     try:
+#         chosen_input = int(input("Enter your choice [1/2/3]: "))
+#         if chosen_input == 1:
+#             print("You have chosen One random joke!")
+#             ParseData.parse_data(chosen_input)
+#             break
+#         elif chosen_input == 2:
+#             print("You have chosen Ten random jokes!")
+#             ParseData.parse_data(chosen_input)
+#             break
+#         elif chosen_input == 3:
+#             types = ["general", "programming", "knock-knock"]
+#             numbers = [1, 10]
+#             type_value = None
+#             number = None
+#             while type_value not in types:
+#                 type_value = str(input("Please choose a type [general|programming|knock-knock]: "))
+#                 if type_value in types:
+#                     break
+#                 else:
+#                     print("Invalid type! Please choose a correct value\n")
+#             print("Your type chosen was {}".format(type_value))
+#             while number not in numbers:
+#                 number = int(input("Please choose the number of jokes [1/10]: "))
+#                 if number in numbers:
+#                     break
+#                 else:
+#                     print("Invalid! Please choose 1 or 10")
+#             ParseData.parse_data(chosen_input, type_value, number)
+#             break
+#         print("Please enter a valid number!\n")
+#     except Exception as e:
+#         print(e, "Please choose a valid input!\n")
+
+# AskInput.ask_choice()
+ParseData.parse_data()
--- testus.py
+++ testus.py
@@ -1,41 +1,46 @@
-from parse_data import ParseData
-print("""
-Hello! Please choose one of the following three inputs:
-1 - One random joke
-2 - Ten random joke
-3 - One/Ten random jokes by type
-""")
-while True:
-    try:
-        chosen_input = int(input("Enter your choice [1/2/3]: "))
-        if chosen_input == 1:
-            print("You have chosen One random joke!")
-            ParseData.parse_data(chosen_input)
-            break
-        elif chosen_input == 2:
-            print("You have chosen Ten random jokes!")
-            ParseData.parse_data(chosen_input)
-            break
-        elif chosen_input == 3:
-            types = ["general", "programming", "knock-knock"]
-            numbers = [1, 10]
-            type_value = None
-            number = None
-            while type_value not in types:
-                type_value = str(input("Please choose a type [general|programming|knock-knock]: "))
-                if type_value in types:
+# from parse_data import ParseData
+
+class AskInput:
+    choice = None
+    chosen_number = None
+    type = None
+
+    @classmethod
+    def ask_choice(cls):
+        while True:
+            try:
+                chosen_input = int(input("Enter your choice [1/2/3]: "))
+                if chosen_input == 1:
+                    print("You have chosen One random joke!")
+                    cls.choice = chosen_input
                     break
-                else:
-                    print("Invalid type! Please choose a correct value\n")
-            print("Your type chosen was {}".format(type_value))
-            while number not in numbers:
-                number = int(input("Please choose the number of jokes [1/10]: "))
-                if number in numbers:
+                elif chosen_input == 2:
+                    print("You have chosen Ten random jokes!")
+                    cls.choice = chosen_input
                     break
-                else:
-                    print("Invalid! Please choose 1 or 10")
-            ParseData.parse_data(chosen_input, type_value, number)
-            break
-        print("Please enter a valid number!\n")
-    except Exception as e:
-        print(e, "Please choose a valid input!\n")
+                elif chosen_input == 3:
+                    cls.choice = chosen_input
+                    types = ["general", "programming", "knock-knock"]
+                    numbers = [1, 10]
+                    type_value = None
+                    nr = None
+                    while type_value not in types:
+                        type_value = str(input("Please choose a type [general|programming|knock-knock]: "))
+                        if type_value in types:
+                            cls.type = type_value
+                            break
+                        else:
+                            print("Invalid type! Please choose a correct value\n")
+                    print("Your type chosen was {}".format(type_value))
+                    while nr not in numbers:
+                        nr = int(input("Please choose the number of jokes [1/10]: "))
+                        if nr in numbers:
+                            cls.chosen_number = nr
+                            print(f"Your number of jokes is {nr}")
+                            break
+                        else:
+                            print("Invalid! Please choose 1 or 10")
+                    break
+                print("Please enter a valid number!\n")
+            except Exception as e:
+                print(e, "Please choose a valid input!\n")
