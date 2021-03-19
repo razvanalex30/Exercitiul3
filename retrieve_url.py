78bd20ad93dd68e68e951e64fa3500e12957846d
RAlexandru <razvan_alex97@yahoo.com>
2021-03-19 10:24:23 +0200

    Modified some comments

--- retrieve_url.py
+++ retrieve_url.py
@@ -1,47 +1,8 @@
 import requests
 
 
-# import random
-
-
-class StatusCode(ValueError):
-    pass
-
-
 class RetrieveUrl:
     base_url = "https://official-joke-api.appspot.com/jokes/"
-
-    # @classmethod
-    # def retrieve_endpoint_1(cls):
-    #     path_choices = ["/random_joke", "/jokes/random"]
-    #     url = "https://official-joke-api.appspot.com"
-    #     path = random.choice(path_choices)
-    #     result = requests.get(url + path)
-    #     joke = result.json()
-    #     return joke
-    #
-    # @classmethod
-    # def retrieve_endpoint_2(cls):
-    #     path_choices = ["/random_ten", "/jokes/ten"]
-    #     url = "https://official-joke-api.appspot.com"
-    #     path = random.choice(path_choices)
-    #     result = requests.get(url + path)
-    #     joke = result.json()
-    #     return joke
-    #
-    # @classmethod
-    # def retrieve_endpoint_3(cls, typee, number):
-    #     url = "https://official-joke-api.appspot.com/jokes/"
-    #     types = ["programming", "general", "knock-knock"]
-    #     if typee in types:
-    #         url += typee
-    #     if number == "1":
-    #         url += "/random"
-    #     else:
-    #         url += "/ten"
-    #     result = requests.get(url)
-    #     joke = result.json()
-    #     return joke
 
     @classmethod
     def retrieve_endpoint(cls, choice, typee=None, number=None):
